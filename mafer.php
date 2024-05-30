a1f1ffffb087502af6925a69f1ceacceb123f4df
MAR2412 <110185907+MAR2412@users.noreply.github.com>
2024-05-30 16:10:22 -0600

    Update mafer.php

--- mafer.php
+++ mafer.php
@@ -1,13 +1,10 @@
 print("Hola")
 print("Hola")
 print("Hola")
-<<<<<<< HEAD
 print("Hola")
-=======
 print("Hola")
 print("Hola")
 print("Hola")
 print("Hola")
 print("Hola")
 print ("Cambios")
->>>>>>> mafer
