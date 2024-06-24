dd368990802d3639cb69aa6991d2d396dcc45a07
Darihana12 <smedinad@unah.hn>
2024-06-23 22:31:03 -0600

    Primer CRUD

---
+++ database/seeders/EstudianteSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Estudiante;
+class EstudianteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Estudiante::factory()->count(500)->create();
+    }
+}
